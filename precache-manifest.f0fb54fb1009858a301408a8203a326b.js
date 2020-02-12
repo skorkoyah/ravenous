aa77615780f7349931f618899c89f5c86bf1f602
skorkoyah <skorkoyah29@gmail.com>
2020-02-12 08:29:27 -0500

    Updates

---
+++ precache-manifest.f0fb54fb1009858a301408a8203a326b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4258f19880eda09c7eca5c41a1829f",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "dd1b8a84a7837bb6aad1",
+    "url": "/ravenous/static/css/main.19ac8a0f.chunk.css"
+  },
+  {
+    "revision": "9eaec0246eced73a68b5",
+    "url": "/ravenous/static/js/2.31aad086.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/ravenous/static/js/2.31aad086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1b8a84a7837bb6aad1",
+    "url": "/ravenous/static/js/main.0041fd1f.chunk.js"
+  },
+  {
+    "revision": "6daa093969d3829a08d5",
+    "url": "/ravenous/static/js/runtime-main.af183450.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
